acb0123cf430988a5c240f053ccfd6d06096c518
Thalles Campagnani <thallescampagnani@gmail.com>
2026-01-16 20:55:44 -0300

    Removendo arquivos antigos, nova biblioteca para variação automática (incompleta ainda)

---
+++ libVariaTarugo.py
@@ -0,0 +1,249 @@
+########################################################################
+####                                                                ####
+####       CENTRO DE DESENVOLVIMENTO DA TECNOLOGIA NUCLEAR          ####
+####                  Simulação do Detector Gama                    ####
+####                           simu.py                              ####
+####                                                                ####
+####             Daniel de Almeida Magalhães Campolina              ####
+####                      Lilly Salim Thein                         ####
+####                 Thalles Oliveira Campagnani                    ####
+####               Jefferson Quintão Campos Duarte                  ####
+####                                                                ####
+########################################################################
+
+import os #para renomear arquivo e limpar tela
+os.system("clear") #Limpa tela
+import numpy as np
+
+
+
+# Funções para escrita em arquivo
+from pprint import pprint
+
+def escreva_comentario(arquivo, valor, nome, comentario = None):
+    # Escreva comantário e salte linha
+    arquivo.write(f"#{comentario}\n")
+
+def escreva_variaveis(arquivo, comentario = None, **kwargs):
+    # Se for passado comentário, escreva antes da variável
+    if comentario != None:
+        arquivo.write(f"#{comentario}\n")
+
+    for nome, valor in kwargs.items():
+        arquivo.write(f"{nome} = ")
+        pprint(valor, stream=arquivo)
+        arquivo.write("\n")
+
+
+
+
+# --- Seção responsável por executar o experimento ---
+
+import libGammaMass
+libGammaMass.simu = True
+libGammaMass.plotar = False
+
+
+def simuVariaSimplesTarugo(
+        # Configurações de variação
+        ini,
+        fin,
+        passo,
+        tipoVaria="area", #Outras opções: proporção, posição, largura, altura, comprimento
+        vetor_varia = [],
+        area = 100, #somente usado no caso de 'proporção'
+        prop = 1, #somente usado no caso area
+
+
+        # Controle de pastas, para se deve voltar uma pasta acima antes de criar
+        voltar=True,
+        
+        # Configurações da simulação
+        particulas = 100000,
+        ciclos     = 100,
+
+        # Configurações de outras geometrias e fonte
+        ## Parâmetros do tarugo (Substituidos se gerados automaticamente)
+        tarugo_esteira_pos = 0, #centralizado em cima da fonte
+        tarugo_comprimento = 10,
+        tarugo_largura     = 10,
+        tarugo_altura      = 10,
+
+        ## Parâmetros do colimador
+        colimador_espessura = 2.7, #Espessura nominal do colimador LB-4700
+        colimador_abertura  = 7.8, #Diametro do detector
+        colimador_impureza  = 0,
+        
+        ## Parãmetros das fontes
+        fonte_cobalto_intensidade    = 7.4e6, ########### Atividade de 3.7e7 x2 pois são 2 fótons
+        fonte_raiosCosmicos_mes      = 1,
+        fonte_raiosCosmicos_latitude = 0,
+        
+        ## Parametros do detector
+        detectores_numero           = 1,
+        detectores_altura_meio      = 52.35,
+        detectores_altura_esquerda  = 52.35,
+        detectores_altura_direita   = 52.35,
+
+        ##Parametros concreto
+        fonte_Concreto_intensidade = 1.59e4           ### multiplicado por 1,57 área da semi-esfera
+        ):
+    
+    # Cria pasta com data no nome para armazenar os resultados
+    libGammaMass.mkdir("resultados_"+str(int(fonte_cobalto_intensidade))+"_"+str(int(colimador_espessura*10)), data=False, voltar=voltar)
+
+    if tipoVaria == "proporção":
+        #Gera sequencia inversamente simetrica
+        sequencia_lado_direito = np.arange(ini, fin + (passo / 1000.0), passo)
+        sequencia_lado_esquerdo = np.sort(1 / sequencia_lado_direito[1:])
+        vetor_varia = np.concatenate((sequencia_lado_esquerdo, sequencia_lado_direito))
+    else:
+        vetor_varia = range(ini, fin+1, passo)
+
+
+
+
+    for varia in vetor_varia:
+        libGammaMass.mkdir(f"simulação.{area}", data=False, voltar=(varia!=vetor_varia[0]))#Voltar é false apenas para a primeira execução
+
+        print("################")
+        print("######## Rodando caso: ")
+        print(f"####### {tipoVaria} = {varia} ")
+        print("################")
+
+        
+        if tipoVaria == "area":
+            tarugo_largura      = np.sqrt(vetor_varia)*prop,
+            tarugo_altura       = np.sqrt(vetor_varia)/prop,
+
+        elif tipoVaria == "proporção":
+            tarugo_largura  = np.sqrt(area)*vetor_varia,
+            tarugo_altura   = np.sqrt(area)/vetor_varia,
+
+        elif tipoVaria == "posição":
+            tarugo_esteira_pos           = tarugo_esteira_pos
+
+        elif tipoVaria == "largura":
+            tarugo_largura               = tarugo_largura
+
+        elif tipoVaria == "altura":
+            tarugo_altura                = tarugo_altura,
+
+        elif tipoVaria == "comprimento":
+            tarugo_comprimento = tarugo_comprimento
+
+        else:
+            exit(1)
+
+
+
+
+        # Criando reator no OpenMC
+        detector = libGammaMass.Detector()
+        # Alterando Geometria
+        detector.geometria(
+            # Valores gerados de acordo com os parâmetros passados como parâmetros
+            tarugo_largura               = tarugo_largura,
+            tarugo_altura                = tarugo_altura,
+            tarugo_comprimento           = tarugo_comprimento,
+            tarugo_esteira_pos           = tarugo_esteira_pos,
+            # Repassar valores dos parâmetros
+            colimador_espessura          = colimador_espessura,
+            colimador_abertura           = colimador_abertura,
+            colimador_impureza           = colimador_impureza,
+            fonte_cobalto_intensidade    = fonte_cobalto_intensidade,
+            fonte_raiosCosmicos_mes      = fonte_raiosCosmicos_mes,
+            fonte_raiosCosmicos_latitude = fonte_raiosCosmicos_latitude,
+            fonte_Concreto_intensidade   = fonte_Concreto_intensidade,
+            detectores_numero            = detectores_numero,
+            detectores_altura_meio       = detectores_altura_meio,
+            detectores_altura_esquerda   = detectores_altura_esquerda,
+            detectores_altura_direita    = detectores_altura_direita
+        )
+        # Rodando configurações novamente para atualizar a fonte da simulação e alterar as configurações de simulação
+        detector.configurações(particulas=particulas, ciclos=ciclos)
+
+        detector.plotagem("plot.lateral.xy.png",  "xy")
+        detector.plotagem("plot.frontal.yz.png",  "yz", rotacionar = True)
+        #detector.plotagem("plot.superior.xz.png", "xz", rotacionar = True, origin=(0,52.35,0))
+
+
+        intervalos_energias=np.linspace(5e3,2e6,2**10).tolist() # Intervalo de 5KeV a 2MeV dividido em 2¹⁰ canais
+        
+        #Configurações de tallies
+        detector.tallies(init=True)     #Iniciar a lista de tallies
+        detector.tallies_detector(energia=intervalos_energias, score="flux",         nome="espectroFluxo")           #1
+        detector.tallies_detector(energia=intervalos_energias, score="pulse-height", nome="espectroPulso")           #2
+        detector.tallies(export=True)   #Finalizar a lista (exportar tallies.xml)
+
+
+        detector.simular()
+
+
+
+
+
+    if libGammaMass.simu == True:
+
+        # --- Seção responsável por extrair resultados dos experimentos ---
+
+        libGammaMass.chdir("..")
+
+        vetor_fluxo = [] #vetor para salvar todos os resultados
+        vetor_fluxo_incerteza = [] #vetor de incertezas (na verdade é o desvio padrão (std))
+        vetor_pulso = [] #vetor para salvar todos os resultados
+        vetor_pulso_incerteza = [] #vetor de incertezas (na verdade é o desvio padrão (std))
+        # Navegue arquivo por arquivo coletando os resultados
+        for i in range(area_ini, area_fin+1, passo):
+            fluxo, incerteza_f =     detector.tallies_detector(get=True,    nome="espectroFluxo", score="flux",          file=f"simulação.{i}/statepoint.{config[1]}.h5")
+            pulso, incerteza_p =     detector.tallies_detector(get=True,    nome="espectroPulso", score="pulse-height",  file=f"simulação.{i}/statepoint.{config[1]}.h5")
+            
+            #Salve eles nos vetores
+            vetor_fluxo.append(fluxo)
+            vetor_fluxo_incerteza.append(incerteza_f)
+            vetor_pulso.append(pulso)
+            vetor_pulso_incerteza.append(incerteza_p)
+
+
+
+
+
+
+
+        # --- Seção responsável por salvar entradas e saídas de todos experimentos ---
+
+
+        with open("resultados_simuVariaArea.py", "w") as f:  #### mudar nome para separar os casos
+            escreva_comentario(f," Resultados da Simulação OpenMC - Função simuVariaArea\n\n")
+            escreva_comentario(f," Arquivo gerado automaticamente\n\n")
+            escreva_variaveis(f," Configurações de simulação:", particulas=particulas,  ciclos=ciclos)
+            escreva_variaveis(f," Configurações de variação de área:", area_ini=area_ini, area_fin=area_fin, passo=passo)
+            escreva_comentario(f," Configurações de geometria e fonte:")
+            escreva_variaveis(f,"# Parâmetros do tarugo (tarugo_largura e tarugo_altura são gerados automaticamente):", tarugo_esteira_pos=tarugo_esteira_pos, tarugo_comprimento=tarugo_comprimento)
+            escreva_variaveis(f,"# Parâmetros do colimador:",  colimador_espessura=colimador_espessura, colimador_abertura=colimador_abertura, colimador_impureza=colimador_impureza)
+            escreva_variaveis(f,"# Parãmetros das fontes", fonte_cobalto_intensidade=fonte_cobalto_intensidade, fonte_raiosCosmicos_mes=fonte_raiosCosmicos_mes, fonte_raiosCosmicos_latitude=fonte_raiosCosmicos_latitude)
+            escreva_variaveis(f,"# Parametros do detector", detectores_numero=detectores_numero, detectores_altura_meio=detectores_altura_meio, detectores_altura_esquerda=detectores_altura_esquerda, detectores_altura_direita=detectores_altura_direita)
+            escreva_variaveis(f,"# Parametros concreto", fonte_Concreto_intensidade=fonte_Concreto_intensidade)
+            escreva_comentario(f," Espectro para resultados:")
+            escreva_variaveis(f," Intervalos de energias que são divididos os tallies de fluxo e altura de pulso", intervalos_energias=intervalos_energias)
+            escreva_comentario(f," Resultados:\n\n")
+            escreva_variaveis(f,"# Vetor contendo cada área simulada",vetor_area=vetor_area)
+            escreva_variaveis(f,"# Vetor contendo o espectro de fluxo para cada área simulada",vetor_fluxo=vetor_fluxo)
+            escreva_variaveis(f,"# Vetor contendo o espectro de incerteza do fluxo para cada área simulada",vetor_fluxo_incerteza=vetor_fluxo_incerteza)
+            escreva_variaveis(f,"# Vetor contendo o espectro de pulso para cada área simulada",vetor_pulso=vetor_pulso)
+            escreva_variaveis(f,"# Vetor contendo o espectro de incerteza do pulso para cada área simulada",vetor_pulso_incerteza=vetor_pulso_incerteza)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- simuGammaMass.py
+++ simuGammaMass.py
@@ -13,155 +13,8 @@
 
 import os #para renomear arquivo e limpar tela
 os.system("clear") #Limpa tela
-import numpy as np
 
-# Funções para escrita em arquivo
-from pprint import pprint
-
-def escreva_comentario(arquivo, valor, nome, comentario = None):
-    # Escreva comantário e salte linha
-    arquivo.write(f"#{comentario}\n")
-
-def escreva_variaveis(arquivo, comentario = None, **kwargs):
-    # Se for passado comentário, escreva antes da variável
-    if comentario != None:
-        arquivo.write(f"#{comentario}\n")
-
-    for nome, valor in kwargs.items():
-        arquivo.write(f"{nome} = ")
-        pprint(valor, stream=arquivo)
-        arquivo.write("\n")
-
-
-
-
-# --- Seção responsável por executar o experimento ---
-
-import libGammaMass
-libGammaMass.simu = True
-libGammaMass.plotar = False
-
-
-def simuVariaArea(
-        # Controle de pastas, para se deve voltar uma pasta acima antes de criar
-        voltar=True,
-        
-        # Configurações da simulação
-        particulas = 100000,
-        ciclos     = 100,
-
-        # Configurações de variação de área
-        area_ini = 0,
-        area_fin = 625, #25x25
-        passo    = 25,
-
-        # Configurações de geometria e fonte
-        ## Parâmetros do tarugo
-        tarugo_esteira_pos = 0, #centralizado em cima da fonte
-        tarugo_comprimento = 10,
-        #Gerados automaticamente:
-        #tarugo_largura     = 10,
-        #tarugo_altura      = 10,
-        
-        ## Parâmetros do colimador
-        colimador_espessura = 2.7, #Espessura nominal do colimador LB-4700
-        colimador_abertura  = 7.8, #Diametro do detector
-        colimador_impureza  = 0,
-        
-        ## Parãmetros das fontes
-        fonte_cobalto_intensidade    = 7.4e6, ########### Atividade de 3.7e7 x2 pois são 2 fótons
-        fonte_raiosCosmicos_mes      = 1,
-        fonte_raiosCosmicos_latitude = 0,
-        
-        ## Parametros do detector
-        detectores_numero           = 1,
-        detectores_altura_meio      = 52.35,
-        detectores_altura_esquerda  = 52.35,
-        detectores_altura_direita   = 52.35,
-
-        ##Parametros concreto
-        fonte_Concreto_intensidade = 1.59e4           ### multiplicado por 1,57 área da semi-esfera
-        ):
-    # Cria pasta com data no nome para armazenar os resultados
-    libGammaMass.mkdir("resultados_"+str(int(fonte_cobalto_intensidade))+"_"+str(int(colimador_espessura*10)), data=False, voltar=voltar)
-
-    #Configurações da simulação
-    config = [particulas, ciclos] #Particulas, ciclos totais
-
-
-    vetor_area = []
-    for area in range(area_ini, area_fin+1, passo):
-        libGammaMass.mkdir(f"simulação.{area}", data=False, voltar=(area!=area_ini))
-
-        print("################")
-        print("####### ",area, " ######")
-        print("################")
-
-        
-        #Criando reator no OpenMC
-        vetor_area.append(area)
-        detector = libGammaMass.Detector(particulas=config[0], ciclos=config[1])
-        detector.geometria(
-            # Valores gerados de acordo com os parâmetros passados como parâmetros
-            tarugo_largura               = np.sqrt(area),
-            tarugo_altura                = np.sqrt(area),
-            # Repassar valores dos parâmetros
-            tarugo_esteira_pos           = tarugo_esteira_pos,
-            tarugo_comprimento           = tarugo_comprimento,
-            colimador_espessura          = colimador_espessura,
-            colimador_abertura           = colimador_abertura,
-            colimador_impureza           = colimador_impureza,
-            fonte_cobalto_intensidade    = fonte_cobalto_intensidade,
-            fonte_raiosCosmicos_mes      = fonte_raiosCosmicos_mes,
-            fonte_raiosCosmicos_latitude = fonte_raiosCosmicos_latitude,
-            fonte_Concreto_intensidade   = fonte_Concreto_intensidade,
-            detectores_numero            = detectores_numero,
-            detectores_altura_meio       = detectores_altura_meio,
-            detectores_altura_esquerda   = detectores_altura_esquerda,
-            detectores_altura_direita    = detectores_altura_direita
-        )
-        detector.configurações(particulas=config[0], ciclos=config[1])
-
-        detector.plotagem("plot.lateral.xy.png",  "xy")
-        detector.plotagem("plot.frontal.yz.png",  "yz", rotacionar = True)
-        #detector.plotagem("plot.superior.xz.png", "xz", rotacionar = True, origin=(0,52.35,0))
-
-
-        intervalos_energias=np.linspace(5e3,2e6,2**10).tolist() # Intervalo de 5KeV a 2MeV dividido em 2¹⁰ canais
-        
-        #Configurações de tallies
-        detector.tallies(init=True)     #Iniciar a lista de tallies
-        detector.tallies_detector(energia=intervalos_energias, score="flux",         nome="espectroFluxo")           #1
-        detector.tallies_detector(energia=intervalos_energias, score="pulse-height", nome="espectroPulso")           #2
-        detector.tallies(export=True)   #Finalizar a lista (exportar tallies.xml)
-
-
-        detector.simular()
-
-
-
-
-
-    if libGammaMass.simu == True:
-
-        # --- Seção responsável por extrair resultados dos experimentos ---
-
-        libGammaMass.chdir("..")
-
-        vetor_fluxo = [] #vetor para salvar todos os resultados
-        vetor_fluxo_incerteza = [] #vetor de incertezas (na verdade é o desvio padrão (std))
-        vetor_pulso = [] #vetor para salvar todos os resultados
-        vetor_pulso_incerteza = [] #vetor de incertezas (na verdade é o desvio padrão (std))
-        # Navegue arquivo por arquivo coletando os resultados
-        for i in range(area_ini, area_fin+1, passo):
-            fluxo, incerteza_f =     detector.tallies_detector(get=True,    nome="espectroFluxo", score="flux",          file=f"simulação.{i}/statepoint.{config[1]}.h5")
-            pulso, incerteza_p =     detector.tallies_detector(get=True,    nome="espectroPulso", score="pulse-height",  file=f"simulação.{i}/statepoint.{config[1]}.h5")
-            
-            #Salve eles nos vetores
-            vetor_fluxo.append(fluxo)
-            vetor_fluxo_incerteza.append(incerteza_f)
-            vetor_pulso.append(pulso)
-            vetor_pulso_incerteza.append(incerteza_p)
+import libVariaTarugo
 
 
 
@@ -169,53 +22,67 @@ def simuVariaArea(
 
 
 
-        # --- Seção responsável por salvar entradas e saídas de todos experimentos ---
-
-
-        with open("resultados_simuVariaArea.py", "w") as f:  #### mudar nome para separar os casos
-            escreva_comentario(f," Resultados da Simulação OpenMC - Função simuVariaArea\n\n")
-            escreva_comentario(f," Arquivo gerado automaticamente\n\n")
-            escreva_variaveis(f," Configurações de simulação:", particulas=particulas,  ciclos=ciclos)
-            escreva_variaveis(f," Configurações de variação de área:", area_ini=area_ini, area_fin=area_fin, passo=passo)
-            escreva_comentario(f," Configurações de geometria e fonte:")
-            escreva_variaveis(f,"# Parâmetros do tarugo (tarugo_largura e tarugo_altura são gerados automaticamente):", tarugo_esteira_pos=tarugo_esteira_pos, tarugo_comprimento=tarugo_comprimento)
-            escreva_variaveis(f,"# Parâmetros do colimador:",  colimador_espessura=colimador_espessura, colimador_abertura=colimador_abertura, colimador_impureza=colimador_impureza)
-            escreva_variaveis(f,"# Parãmetros das fontes", fonte_cobalto_intensidade=fonte_cobalto_intensidade, fonte_raiosCosmicos_mes=fonte_raiosCosmicos_mes, fonte_raiosCosmicos_latitude=fonte_raiosCosmicos_latitude)
-            escreva_variaveis(f,"# Parametros do detector", detectores_numero=detectores_numero, detectores_altura_meio=detectores_altura_meio, detectores_altura_esquerda=detectores_altura_esquerda, detectores_altura_direita=detectores_altura_direita)
-            escreva_variaveis(f,"# Parametros concreto", fonte_Concreto_intensidade=fonte_Concreto_intensidade)
-            escreva_comentario(f," Espectro para resultados:")
-            escreva_variaveis(f," Intervalos de energias que são divididos os tallies de fluxo e altura de pulso", intervalos_energias=intervalos_energias)
-            escreva_comentario(f," Resultados:\n\n")
-            escreva_variaveis(f,"# Vetor contendo cada área simulada",vetor_area=vetor_area)
-            escreva_variaveis(f,"# Vetor contendo o espectro de fluxo para cada área simulada",vetor_fluxo=vetor_fluxo)
-            escreva_variaveis(f,"# Vetor contendo o espectro de incerteza do fluxo para cada área simulada",vetor_fluxo_incerteza=vetor_fluxo_incerteza)
-            escreva_variaveis(f,"# Vetor contendo o espectro de pulso para cada área simulada",vetor_pulso=vetor_pulso)
-            escreva_variaveis(f,"# Vetor contendo o espectro de incerteza do pulso para cada área simulada",vetor_pulso_incerteza=vetor_pulso_incerteza)
 
 
 
 
-#Começo da execução
-libGammaMass.mkdir("resultados_grupo_varia_intensidade_especura", data=True)
 
-#resultados gerados
-simuVariaArea(fonte_cobalto_intensidade=0,     colimador_espessura = 2.7, voltar=False)
-simuVariaArea(fonte_cobalto_intensidade=7.4e1, colimador_espessura = 2.7)
-simuVariaArea(fonte_cobalto_intensidade=7.4e2, colimador_espessura = 2.7)
-simuVariaArea(fonte_cobalto_intensidade=7.4e3, colimador_espessura = 2.7)
-simuVariaArea(fonte_cobalto_intensidade=7.4e4, colimador_espessura = 2.7)
-simuVariaArea(fonte_cobalto_intensidade=7.4e5, colimador_espessura = 2.7)
-simuVariaArea(fonte_cobalto_intensidade=7.4e6, colimador_espessura = 2.7)
-simuVariaArea(fonte_cobalto_intensidade=7.4e7, colimador_espessura = 2.7)
+# Cria pasta para armazenar todos os resultados, com data no nome
+libVariaTarugo.libGammaMass.mkdir("resultados", data=True)
 
-simuVariaArea(fonte_cobalto_intensidade=7.4e7, colimador_espessura = 0)
-simuVariaArea(fonte_cobalto_intensidade=7.4e6, colimador_espessura = 0)
-simuVariaArea(fonte_cobalto_intensidade=7.4e5, colimador_espessura = 0)
-simuVariaArea(fonte_cobalto_intensidade=7.4e4, colimador_espessura = 0)
-simuVariaArea(fonte_cobalto_intensidade=7.4e3, colimador_espessura = 0)
-simuVariaArea(fonte_cobalto_intensidade=7.4e2, colimador_espessura = 0)
-simuVariaArea(fonte_cobalto_intensidade=7.4e1, colimador_espessura = 0)
-simuVariaArea(fonte_cobalto_intensidade=0,     colimador_espessura = 0)
+
+# Caso Variação de Área com restante dos parametros fixos:
+## Neste caso a área da sessão transversal é variada de 0 a 625 em todas simulações,
+## sendo possível analizar o espectro de energia do fluxo que chega ao detector e também o espectro de energia dos pulsos gerados.
+## Este "for" abaixo repete a simulação com várias magnitudes de intensidade de fonte, com ou sem bindagem.
+## Para cada execução do simuVariaSimplesTarugo é possível gerar 1 espectro de fluxo e 1 espectro de pulso para cada área simulada internamente.
+## Para cada execução do simuVariaSimplesTarugo é possível gerar 1 curva de "fluxo vs. area" e 1 curva de "pulso vs. area" trabalhando dados de cada espectro referente a cada area
+
+libVariaTarugo.libGammaMass.mkdir("resultados_grupo_variaArea_intensidade_especura", voltar=False)
+voltar = True
+for fonte_cobalto_intensidade in [0, 7.4e1, 7.4e2, 7.4e3, 7.4e4, 7.4e5, 7.4e6, 7.4e7]:
+    libVariaTarugo.simuVariaSimplesTarugo(tipoVaria="area", ini=0, fin=625, passo=25, prop=1, fonte_cobalto_intensidade=fonte_cobalto_intensidade, colimador_espessura = 2.7, voltar=voltar)
+    libVariaTarugo.simuVariaSimplesTarugo(tipoVaria="area", ini=0, fin=625, passo=25, prop=1, fonte_cobalto_intensidade=fonte_cobalto_intensidade, colimador_espessura = 0)
+    voltar=False
+
+
+
+
+# Caso Variação de Proporção com restante dos parametros fixos:
+## Neste caso a área da sessão transversal é mantida, e é variada a proporção de 1/2 a 1 e 1 a 2 por padrão em todas simulações,
+## sendo possível analizar o espectro de energia do fluxo que chega ao detector e também dos pulsos gerados.
+## Este for abaixo repete a simulação com várias magnitudes de intensidade de fonte, com ou sem bindagem.
+## Para cada execução do simuVariaProporcao é possível gerar 1 espectro de fluxo e 1 espectro de pulso para cada proporção.
+## Para cada execução do simuVariaProporcao é possível gerar 1 curva de "fluxo vs. proporção" e 1 curva de "pulso vs. proporção"
+
+libVariaTarugo.libGammaMass.mkdir("resultados_grupo_variaProp_intensidade_especura")
+voltar = True
+for fonte_cobalto_intensidade in [0, 7.4e1, 7.4e2, 7.4e3, 7.4e4, 7.4e5, 7.4e6, 7.4e7]:
+    libVariaTarugo.simuVariaProporcao(tipoVaria="prop", ini=1, fin=2, passo=0.2, area=200, fonte_cobalto_intensidade=0, colimador_espessura = 2.7, voltar=voltar)
+    libVariaTarugo.simuVariaProporcao(tipoVaria="prop", ini=1, fin=2, passo=0.2, area=200, fonte_cobalto_intensidade=0, colimador_espessura = 0)
+    voltar=False
+
+
+# Caso Variação de Comprimento com restante dos parametros fixos:
+## Neste caso a área da sessão transversal e proporção são mantidas, e é variada o comprimento de 0 a  por padrão em todas simulações,
+## sendo possível analizar o espectro de energia do fluxo que chega ao detector e também dos pulsos gerados.
+## Este for abaixo repete a simulação com várias magnitudes de intensidade de fonte, com ou sem bindagem.
+## Para cada execução do simuVariaProporcao é possível gerar 1 espectro de fluxo e 1 espectro de pulso para cada proporção.
+## Para cada execução do simuVariaProporcao é possível gerar 1 curva de "fluxo vs. proporção" e 1 curva de "pulso vs. proporção"
+
+libVariaTarugo.libGammaMass.mkdir("resultados_grupo_variaComp_intensidade_especura")
+voltar = True
+for fonte_cobalto_intensidade in [0, 7.4e1, 7.4e2, 7.4e3, 7.4e4, 7.4e5, 7.4e6, 7.4e7]:
+    libVariaTarugo.simuVariaProporcao(tipoVaria="comp", ini=0, fin=1000, passo=100, area=200, fonte_cobalto_intensidade=0, colimador_espessura = 2.7, voltar=voltar)
+    libVariaTarugo.simuVariaProporcao(tipoVaria="comp", ini=0, fin=10000, passo=1000, area=200, fonte_cobalto_intensidade=0, colimador_espessura = 2.7, voltar=voltar)
+    libVariaTarugo.simuVariaProporcao(tipoVaria="comp", ini=0, fin=1000, passo=100, area=200, fonte_cobalto_intensidade=0, colimador_espessura = 0)
+    libVariaTarugo.simuVariaProporcao(tipoVaria="comp", ini=0, fin=10000, passo=1000, area=200, fonte_cobalto_intensidade=0, colimador_espessura = 0)
+    voltar=False
+
+
+
+
+
 
 
 # ToDo List:
